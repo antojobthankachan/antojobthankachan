e65b12e26caf893123256f81358fbf499fb7adee
antojobthankachan <84390773+antojobthankachan@users.noreply.github.com>
2021-05-18 18:36:29 +0530

    Add files via upload

---
+++ scripts/mainf269.js
@@ -0,0 +1,1 @@
+AOS.init({anchorPlacement:'top-left',duration:1000,});var $grid=$('.grid').masonry({itemSelector:'.grid-item',percentPosition:true,horizontalOrder:true});$grid.imagesLoaded().progress(function(){$grid.masonry();});
